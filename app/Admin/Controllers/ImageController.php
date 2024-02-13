2e71a5d8617886c0004005462a9c0f4d839e24e8
fidaahammoud <fidaahammoud2003@gmail.com>
2024-02-13 13:36:16 +0000

    add recipe , image and user admin controllers

---
+++ app/Admin/Controllers/ImageController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use OpenAdmin\Admin\Controllers\AdminController;
+use OpenAdmin\Admin\Form;
+use OpenAdmin\Admin\Grid;
+use OpenAdmin\Admin\Show;
+use \App\Models\Image;
+
+class ImageController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Image';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Image());
+
+        $grid->column('id', __('Id'));
+        $grid->column('image', __('Image'));
+        $grid->column('type', __('Type'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Image::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('image', __('Image'));
+        $show->field('type', __('Type'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Image());
+
+        $form->image('image', __('Image'));
+        $form->text('type', __('Type'));
+
+        return $form;
+    }
+}
---
+++ app/Admin/Controllers/RecipeController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use OpenAdmin\Admin\Controllers\AdminController;
+use OpenAdmin\Admin\Form;
+use OpenAdmin\Admin\Grid;
+use OpenAdmin\Admin\Show;
+use \App\Models\Recipe;
+use App\Models\Category;
+use App\Models\User;
+
+class RecipeController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Recipe';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Recipe());
+
+        $grid->column('id', __('Id'));
+        //$grid->column('creator_id', __('Creator id'));
+        $grid->column('user.name', __('CreatorName'));
+        $grid->column('title', __('Title'));
+        $grid->column('category.name', __('Category')); // Accessing category name through the relationship
+        //$grid->column('category_id', __('Category id'));
+        $grid->column('description', __('Description'));
+        //$grid->column('image_id', __('Image id'));
+        $grid->column('image.image', __('Image')); 
+        $grid->column('preparationTime', __('PreparationTime'));
+        $grid->column('comment', __('Comment'));
+        $grid->column('totalLikes', __('TotalLikes'));
+        $grid->column('avrgRating', __('AvrgRating'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+
+        $grid->filter(function($filter) {
+            $filter->equal('category_id', 'Category')->select(Category::pluck('name', 'id'));
+        });
+
+        // Filter by creator id
+        $grid->filter(function($filter) {
+            $filter->equal('creator_id', 'Creator ID')->select(User::pluck('name', 'id'));
+        });
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Recipe::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('creator_id', __('Creator id'));
+        $show->field('title', __('Title'));
+        $show->field('category_id', __('Category id'));
+        $show->field('description', __('Description'));
+        $show->field('image_id', __('Image id'));
+        $show->field('preparationTime', __('PreparationTime'));
+        $show->field('comment', __('Comment'));
+        $show->field('totalLikes', __('TotalLikes'));
+        $show->field('avrgRating', __('AvrgRating'));
+        $show->field('created_at', __('Created at'));
+        $show->field('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Recipe());
+
+        $form->number('creator_id', __('Creator id'));
+        $form->text('title', __('Title'));
+        $form->number('category_id', __('Category id'));
+        $form->textarea('description', __('Description'));
+        $form->number('image_id', __('Image id'));
+        $form->number('preparationTime', __('PreparationTime'));
+        $form->textarea('comment', __('Comment'));
+        $form->number('totalLikes', __('TotalLikes'));
+        $form->decimal('avrgRating', __('AvrgRating'))->default(0.00);
+
+        return $form;
+    }
+}
