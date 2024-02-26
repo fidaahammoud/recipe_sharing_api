c9aed4ca82c05feca72dd488e26743b533fc3625
fidaahammoud <fidaahammoud2003@gmail.com>
2024-02-26 11:56:49 +0000

    Display recipes on the category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -19,7 +19,7 @@ class CategoryController extends Controller
 
     public function show(Category $category)
     {
-        $category->load('recipes');
+        $category->load('recipes.user.images','recipes.images');
 
         return $category;
     }
